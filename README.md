e7d105db0b2d1e81c271c7126e775188a0047632
jorherrdz <jorseb21@gmail.com>
2026-09-22 10:00:18 -0700

    i made a edit to readme and added to a file to analysis

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ this is a demo for stat 545
 
 Jorge Hernandez Rodriguez
 Masters Experimental Medicine
+
+
+this a edit made on RStudio
---
+++ analysis/analysis1.R
@@ -0,0 +1,8 @@
+#view teh code if mtcar dataset
+head(mtcars)
+
+#calculate the mean mpg all cars and save it as a variable
+mean_mpg <- mean(mtcars$mpg)
+
+#print the mean mpg
+mean_mpg
